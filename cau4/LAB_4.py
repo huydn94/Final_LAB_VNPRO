fa3fd0ce9ad8740c1cf6d0eb30e0454a823ac436
huydn94 <huydang.1094@gmail.com>
2023-06-12 16:19:10 +0700

    test

---
+++ cau4/LAB_4.py
@@ -0,0 +1,24 @@
+import requests
+import json
+requests.packages.urllib3.disable_warnings()
+baseUrl = "https://api.meraki.com/api/v1"
+API_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+org_id = "681155"
+def get_org():
+    url = baseUrl + f"/organizations/{org_id}/inventory/devices"
+    header = {
+        "X-Cisco-Meraki-API-Key" : API_key
+    }
+    response= requests.get(url,headers=header,verify=False)
+    data=response.json()
+    parse_data=json.dumps(data,indent=4)
+    # print(data)
+    # print(type(data))
+    device_null=[]
+    for i in data:
+        if i['networkId']==None:
+            device_null.append(i) 
+    
+    print(json.dumps(device_null, indent=4))
+if __name__ == "__main__":
+    get_org()
